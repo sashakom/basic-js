4b19aa4a873ffae4f01cd5a45ec89ede27f417bb
Oleksandr Komar <35637943+sashakom@users.noreply.github.com>
2020-02-27 21:03:54 +0200

    init: start task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let result = '';
+  members.sort();
+  members.forEach((item) => {
+    if (typeof(item) === 'string') {
+      result += item[0].toUpperCase();
+    } else {
+      return false;
+    }
+  });
+  return result;
+}
