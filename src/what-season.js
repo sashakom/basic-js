1db48b49e0094263edf88f3c0e73250f357baba6
Oleksandr Komar <35637943+sashakom@users.noreply.github.com>
2020-03-13 21:20:30 +0200

    init: start task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) {
+    return 'winter';
+  } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+    return 'spring';
+  } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+    return 'summer';
+  } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+    return 'autumn';
+  } else {
+    return false;
+  }
+}
